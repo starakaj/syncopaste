c284acd8c75ba8b5ca5521387adb87d9dafe76b4
Sam Tarakajian <starakajian@gmail.com>
2015-09-10 11:42:00 -0400

    Making the spaceship look like a boring triangle, and adding the ability to shoot little pellets

---
+++ Syncopaste/Assets/Scripts/ProjectileShooter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileShooter : MonoBehaviour {
+	
+	public GameObject projectilePrefab;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			var projectileTransform = transform;
+			GameObject projectile = GameObjectUtil.Instantiate(projectilePrefab, projectileTransform.position);
+			var rcolor = new Color(Random.Range(0.2f, 1.0f), Random.Range(0.2f, 1.0f), Random.Range(0.2f, 1.0f), 1.0f);
+			projectile.GetComponent<SpriteRenderer> ().color = rcolor;
+		}
+	}
+}
